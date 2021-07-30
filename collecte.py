fa683c82c17393f76a78a145d653407b3b2fa050
171280 <43889796+171280@users.noreply.github.com>
2021-07-29 23:17:03 -0400

    Add files via upload

---
+++ collecte.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rlkouanda
+#
+# Created:     29-07-2021
+# Copyright:   (c) rlkouanda 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+# Importation des differentes librairies
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
+from sklearn.preprocessing import LabelEncoder
+
+dfClientsPerdus = pd.read_csv("BankChurners_collecte.csv")
+
+# Pour commencer, nous allons modiifer le nom des deux dernièrs prédicteurs car ils sont trop longs.
+dfClientsPerdus.rename(columns={'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1' :\
+                                'NB_mon_1'}, inplace=True)
+dfClientsPerdus.rename(columns={'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2' :\
+                                'NB_mon_2'}, inplace=True)
+
+dfClientsPerdus.info()
+
+print("Avant de commencer l'analyse exploratoire, nous allons supprimer certains prédicteurs.")
+print("1- Le prédicteur CLIENTNUM sera supprimé car il est le numéro unique de chaque client")
+print("2- Les prédicteurs NB_mon_1 et NB_mon_2 seront supprimés")

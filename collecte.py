e594de20eb462552c2fc28c584c10b6a3e3ae0b1
171280 <43889796+171280@users.noreply.github.com>
2021-08-08 19:09:49 -0400

    Update collecte.py
    Ajout d'un message de fin

--- collecte.py
+++ collecte.py
@@ -23,3 +23,4 @@ print("Nous avons 06 prédicteurs qui sont des variables qualitatives")
 #sauvegarder le data
 dfClientsPerdus.to_csv("BankChurners_collecte.csv",index=False)
 
+print("la collecte de données est terminée.")
